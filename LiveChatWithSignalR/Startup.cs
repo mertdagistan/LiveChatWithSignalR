1d5a9c4b17b27e65e86088a43e5df41aec2140d5
Mert Dağıstan <48259052+mertdagistan@users.noreply.github.com>
2019-09-15 22:57:42 +0300

    Importing Project

---
+++ LiveChatWithSignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(LiveChatWithSignalR.Startup))]
+
+namespace LiveChatWithSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            app.MapSignalR();
+        }
+    }
+}
